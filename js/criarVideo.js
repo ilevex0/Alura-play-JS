12390ef6182daf3774adbe5c7838f79deeca333c
Pedro Otávio <pedrootavio09@yahoo.com.br>
2024-08-08 21:07:42 -0300

    criaVideo Função

---
+++ js/criarVideo.js
@@ -0,0 +1,12 @@
+const formulario = document.querySelector("[data-formulario]");
+
+function criarVideo(evento) {
+    evento.preventDefault(); //evita a página recarregar.
+    
+    const imagem = document.querySelector("[data-imagem]").value;
+    const url = document.querySelector("[data-url]").value;
+    const titulo = document.querySelector("[data-titulo]").value;
+    const descricao = Math.floor(Math.random() * 10).toString();
+}
+
+formulario.addEventListener("submit", evento => criarVideo(evento));
